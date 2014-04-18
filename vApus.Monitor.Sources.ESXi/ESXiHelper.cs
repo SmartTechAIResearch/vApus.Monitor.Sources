2346d8930d0e259c74814e3bfc3d1b1f580281ea
Dieter Vandroemme <dieter@sizingservers.be>
2014-04-18 15:17:24 +0200

    Wrapping my head around the vsphere sdk. Nearly there.

--- vApus.Monitor.Sources.ESXi/ESXiHelper.cs
+++ vApus.Monitor.Sources.ESXi/ESXiHelper.cs
@@ -26,7 +26,7 @@ namespace vApus.Monitor.Sources.ESXi {
         /// <param name="ipadress"></param>
         /// <param name="username"></param>
         /// <param name="password"></param>
-        public override bool Connect() {
+        public bool Connect() {
             //Accept all (self-signed) ssl certificates.
             ServicePointManager.ServerCertificateValidationCallback += new RemoteCertificateValidationCallback((sender, certificate, chain, policyErrors) => { return true; });
 
@@ -51,9 +51,7 @@ namespace vApus.Monitor.Sources.ESXi {
                 return false;
             }
 
-
             ServiceContent _serviceContent = _service.RetrieveServiceContent(siMOR);
-
 
             IPAddress address;
             if (IPAddress.TryParse(HostNameOrIPAddress, out address)) {
@@ -62,7 +60,6 @@ namespace vApus.Monitor.Sources.ESXi {
                 IPHostEntry entry = Dns.GetHostEntry(HostNameOrIPAddress);
                 _hostRef = _service.FindByDnsName(_serviceContent.searchIndex, null, entry.HostName, false);
             }
-
 
             _perfProviderSummary = _service.QueryPerfProviderSummary(_serviceContent.perfManager, _hostRef);
 
@@ -88,30 +85,54 @@ namespace vApus.Monitor.Sources.ESXi {
         /// Returns the HostHardwareInfo
         /// </summary>
         /// <returns></returns>
-        public HostHardwareInfo getHardwareInfo() {
+        public HostHardwareInfo GetHostHardwareInfo() {
             ObjectContent[] results = GetPropertyContent("HostSystem", "hardware", _hostRef);
-
-            //check if these are the perfIntervals
-            if (results.Length > 0 && results[0].propSet != null && results[0].propSet[0].name.Equals("hardware"))
-                return (HostHardwareInfo)results[0].propSet[0].val;
-
-            return null;
+            return (HostHardwareInfo)results[0].propSet[0].val;
         }
 
-        private List<int> getPerfCounterIdsForEntity(ManagedObjectReference entityRef) {
+        private ManagedObjectReference[] GetVMs() {
+            //First ask the childentity from the rootfolder (datacenter)
+            ObjectContent[] oCont = GetPropertyContent("Folder", "childEntity", _serviceContent.rootFolder);
+
+            ManagedObjectReference datacenter = ((ManagedObjectReference[])oCont[0].propSet[0].val)[0];
+
+            //Then ask the vm folder from the datacenter
+            oCont = GetPropertyContent("Datacenter", "vmFolder", datacenter);
+            ManagedObjectReference vmFolder = (ManagedObjectReference)oCont[0].propSet[0].val;
+
+            //finally get the list of the managed object from the vms.
+            oCont = GetPropertyContent("Folder", "childEntity", vmFolder);
+            return (ManagedObjectReference[])oCont[0].propSet[0].val;
+        }
+
+        private string GetVMName(ManagedObjectReference vmRef) {
+            ObjectContent[] oCont = GetPropertyContent("VirtualMachine", "name", vmRef);
+            return oCont[0].propSet[0].val.ToString();
+        }
+
+        private VirtualMachinePowerState GetVMPowerState(ManagedObjectReference vmRef) {
+            ObjectContent[] oCont = GetPropertyContent("VirtualMachine", "runtime", vmRef);
+            return ((VirtualMachineRuntimeInfo)oCont[0].propSet[0].val).powerState;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="entityRef">Host or vm ref.</param>
+        /// <returns></returns>
+        private List<int> GetPerfCounterIdsForEntity(ManagedObjectReference entityRef) {
             PerfMetricId[] entityCounters = _service.QueryAvailablePerfMetric(_serviceContent.perfManager, entityRef, DateTime.Now, false, DateTime.Now, false, _perfProviderSummary.refreshRate, true);
             var counterIds = new List<int>();
 
-            foreach (PerfMetricId perfMetric in entityCounters) {
+            foreach (PerfMetricId perfMetric in entityCounters)
                 //to avoid duplicates, there's a WARNING about this because you will loose data 
                 //because the counter can be multiple for multiple instances but since we don't take this in account we can drop this.
                 if (perfMetric.instance == "" && !counterIds.Contains(perfMetric.counterId))
                     counterIds.Add(perfMetric.counterId);
-            }
+
             counterIds.Sort();
             return counterIds;
         }
-
 
         /// <summary>
         /// Method to ask the objectContent[] for a given propertyType, path and reference to the managedobject you're polling
@@ -121,26 +142,13 @@ namespace vApus.Monitor.Sources.ESXi {
         /// <param name="reference"></param>
         /// <returns></returns>
         private ObjectContent[] GetPropertyContent(string propertyType, string path, ManagedObjectReference reference) {
-            PropertySpec spec = new PropertySpec();
-            spec.type = propertyType;
-            spec.pathSet = new string[] { path };
-            PropertySpec[] propertySpecs = new PropertySpec[] { spec };
+            var propertySpecs = new PropertySpec[] { new PropertySpec() { type = propertyType, pathSet = new string[] { path } } };
+            var objectSpecs = new ObjectSpec[] { new ObjectSpec() { obj = reference } };
 
-            //how to retrieve
-            ObjectSpec oSpec = new ObjectSpec();
-            oSpec.obj = reference;
-            ObjectSpec[] objectSpecs = new ObjectSpec[] { oSpec };
-
-            // Create PropertyFilterSpec using the PropertySpec and ObjectPec
-            // created above.
-            PropertyFilterSpec propertyFilterSpec = new PropertyFilterSpec();
-            propertyFilterSpec.propSet = propertySpecs;
-            propertyFilterSpec.objectSet = objectSpecs;
-
-            PropertyFilterSpec[] propertyFilterSpecs = new PropertyFilterSpec[] { propertyFilterSpec };
+            // Create PropertyFilterSpec using the PropertySpec and ObjectPec created above.
+            var propertyFilterSpecs = new PropertyFilterSpec[] { new PropertyFilterSpec() { propSet = propertySpecs, objectSet = objectSpecs } };
 
             return _service.RetrieveProperties(_serviceContent.propertyCollector, propertyFilterSpecs);
         }
-
     }
 }
