3ab6d5891a52e54bd4f8f64272b7310801873d13
Dieter Vandroemme <dieter@sizingservers.be>
2014-10-06 16:26:44 +0200

    Added iostat agent client

---
+++ vApus.Monitor.Sources.IOStat.Agent/IOStatAgentClient.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Copyright 2014 (c) Sizing Servers Lab
+ * University College of West-Flanders, Department GKG
+ * 
+ * Author(s):
+ *    Dieter Vandroemme
+ */
+using vApus.Monitor.Sources.Generic.Agent;
+
+namespace vApus.Monitor.Sources.IOStat.Agent {
+    public class IOStatAgentClient : GenericAgentClient {
+        public IOStatAgentClient()
+            : base() {
+            base.GetParameter("Port").DefaultValue = 5558;
+        }
+    }
+}
