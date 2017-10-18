643ee6129e718767bd9c39102d9551ebabc1da36
Dieter Vandroemme <dieter@sizingservers.be>
2017-10-18 14:05:13 +0200

    Added a monitor source for the proc agent.

---
+++ vApus.Monitor.Sources.Proc.Agent/ProcAgentClient.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Copyright 2017 (c) Sizing Servers Lab
+ * University College of West-Flanders, Department GKG
+ * 
+ * Author(s):
+ *    Dieter Vandroemme
+ */
+using vApus.Monitor.Sources.Generic.Agent;
+
+namespace vApus.Monitor.Sources.Proc.Agent {
+    /// <summary>
+    /// A generic agent implementation having 5556 for a default port to connect to.
+    /// </summary>
+    public class ProcAgentClient : GenericAgentClient {
+        public ProcAgentClient()
+            : base() {
+            base.GetParameter("Port").DefaultValue = 5556;
+        }
+    }
+}
